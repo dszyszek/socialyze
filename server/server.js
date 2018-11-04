935ba837449a50ecb3831e5293c904db54906a0e
Damian <37978110+dszyszek@users.noreply.github.com>
2018-11-04 21:58:39 +0100

    Make mongoose model for testing purposes

--- server/server.js
+++ server/server.js
@@ -12,5 +12,24 @@ let port = process.env.PORT;
 app.use(bodyParser());
 
 
+const model1 = mongoose.model('model1', {
+    text: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const txt = new model1({
+    text: 'Some text',
+    completed: true
+});
+
+txt.save().then(() => console.log('Added successfully')).catch(e => console.log(e));
 
 
