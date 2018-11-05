7d6940e214180475915bac0f0b06d43bf2e5c512
Damian <37978110+dszyszek@users.noreply.github.com>
2018-11-05 19:20:45 +0100

    Add /register route

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const _ = require('lodash');
 
+const {User} = require('../../models/User');
+
+console.log(User.findOne);
 const router = express.Router();
 
 router.get('/test', (req, res) => {
     return res.json({msg: 'Users works'});
+});
+
+router.post('/register', (req, res) => {
+    User.findOne({email: req.body.email}).then(usr => {
+        if (usr) {
+            return res.status(400).json({email: 'Email already exists'});
+        }
+        const userData = _.pick(req.body, ['email', 'name', 'password', 'avatar']);
+        const newUser = new User({
+            email: userData.email,
+            name: userData.name,
+            password: userData.password,
+            avatar: userData.avatar
+        });
+
+        newUser.save().then(usr => (
+            res.send(usr)
+        )).catch(e => console.log(e));
+        
+    }).catch(e => console.log(e));
 });
 
 module.exports = router;
