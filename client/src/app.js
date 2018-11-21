0586ae990dbae1478ee82afdfee2cecad398a714
Damian <37978110+dszyszek@users.noreply.github.com>
2018-11-21 16:17:57 +0100

    Add and setup react

---
+++ client/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss'
+
+// Change name of the app in package.json and index.html!!!
+// Change 'author' in package.json
+// Change the icon (./public/img)!!!
+// Install all the modules so everything is set up!!!
+
+ReactDOM.render(<p>Boilerplate</p>, document.querySelector('#app'));
