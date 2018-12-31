d6235d81d123b378594cb545a127ce6d3656536a
Damian <37978110+dszyszek@users.noreply.github.com>
2018-12-31 19:37:05 +0100

    Make minor changes

--- client/src/app.js
+++ client/src/app.js
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'
 
-// Change name of the app in package.json and index.html!!!
-// Change 'author' in package.json
-// Change the icon (./public/img)!!!
-// Install all the modules so everything is set up!!!
+import AppRouter from './components/routes/AppRouter'
 
-ReactDOM.render(<p>Boilerplate</p>, document.querySelector('#app'));
+
+
+ReactDOM.render(<AppRouter />, document.querySelector('#app'));
