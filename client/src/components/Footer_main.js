fa9919448744a0c43e9b1b2e9e29a3609ab55581
Damian <37978110+dszyszek@users.noreply.github.com>
2019-01-12 03:21:06 +0100

    Add Footer_main component

---
+++ client/src/components/Footer_main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer_main = () => {
+    return (
+        <footer class="mastfoot mt-auto align_bottom">
+            <div class="inner">
+                <p class='text'>Socialyze 2018. Made by DS</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer_main;
