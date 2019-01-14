78db61d66e8e03f4c85e80c72709d004c9628480
Damian <37978110+dszyszek@users.noreply.github.com>
2019-01-14 08:27:12 +0100

    Make minor changes

--- client/src/components/the_rest/LogIn.js
+++ client/src/components/the_rest/LogIn.js
@@ -15,18 +15,17 @@ class LogIn extends React.Component{
 
                         <form class='log-in-form'>
                             <div class="form-group">
-                                <label for="exampleInputEmail1">Email address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                                <label for="Email">Email address</label>
+                                <input type="email" class="form-control" aria-describedby="emailInfo" placeholder="Email..." />
                             </div>
                             <div class="form-group">
-                                <label for="exampleInputPassword1">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                                <label for="Password">Password</label>
+                                <input type="password" class="form-control" placeholder="Password..." />
                             </div>
                             
                             <div class="form-check">
                                 <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                <label class="formCheck" for="checkBox">Check me out</label>
                             </div>
                             <button type="submit" class="aaa btn main_color white_text">Submit</button>
                         
