f8066bca0347f566fd3f504c109dc8ffdb3aedf2
Damian <37978110+dszyszek@users.noreply.github.com>
2019-02-13 23:18:01 +0100

    Add AddExperience component

--- client/src/routes/AppRouter.js
+++ client/src/routes/AppRouter.js
@@ -7,6 +7,7 @@ import LogIn from '../components/the_rest/LogIn';
 import SignUp from '../components/the_rest/SignUp';
 import Dashboard from '../components/the_rest/Dashboard';
 import AddEducation from '../components/the_rest/AddEducation'
+import AddExperience from '../components/the_rest/AddExperience'
 
 import NotFound from '../components/the_rest/NotFound';
 
@@ -20,6 +21,7 @@ const AppRouter = () => (
                 <Route path='/SignUp' component={SignUp} />
                 <Route path='/Dashboard' component={Dashboard} />
                 <Route path='/AddEducation' component={AddEducation} />
+                <Route path='/AddExperience' component={AddExperience} />
 
                 <Route component={NotFound} />
             </Switch>
