4fb9d80a6a5135c6cc16772d6db28093f9e4302f
Damian <37978110+dszyszek@users.noreply.github.com>
2019-02-14 03:13:08 +0100

    Change styling of NotFound component

--- client/src/components/the_rest/NotFound.js
+++ client/src/components/the_rest/NotFound.js
@@ -1,8 +1,26 @@
 import React from 'react';
 
+import Navbar_secondary from './Navbar_secondary';
+import Footer_main from './Footer_main';
+
 const NotFound = () => {
     return (
-        <div>404 NOT FOUND</div>
+        <div class='main_wrapper'>
+            <Navbar_secondary/>
+            <div class='container'>
+                
+                <div class='jumbotron pl-0 pr-0 text-center mt-5'>
+                    <h1 style={{fontSize: '3em'}}>
+                        404 NOT FOUND
+                    </h1>
+                    <p>
+                        It seems like there is no such page :&#40;
+                    </p>
+                </div>
+
+            </div>
+            <Footer_main/>
+        </div>
     );
 };
 
