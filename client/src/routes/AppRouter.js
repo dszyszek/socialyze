6c2ed2a99ff678f327db8aa0f9f26d69eb30568c
Damian <37978110+dszyszek@users.noreply.github.com>
2019-02-18 01:58:21 +0100

    Add redux and create store file

--- client/src/routes/AppRouter.js
+++ client/src/routes/AppRouter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {NavLink, BrowserRouter, Switch, Route} from 'react-router-dom';
+import {Provider} from 'react-redux';
 
 
 import Main from '../components/landing_page/Main';
@@ -13,32 +14,37 @@ import EditProfile from '../components/the_rest/EditProfile'
 import Profile from '../components/the_rest/Profile'
 import Profiles from '../components/the_rest/Profiles'
 import Post from '../components/the_rest/Post'
-
 import NotFound from '../components/the_rest/NotFound';
+
+import store from '../store';
+
 
 const AppRouter = () => (
 
-    <BrowserRouter>
-        <div className='main_wrapper'>
-            <Switch>
-                <Route path='/' component={Main} exact={true} />
-                <Route path='/Login' component={LogIn} />
-                <Route path='/SignUp' component={SignUp} />
-                <Route path='/Dashboard' component={Dashboard} />
-                <Route path='/AddEducation' component={AddEducation} />
-                <Route path='/AddExperience' component={AddExperience} />
-                <Route path='/Feed' component={Feed} />
-                <Route path='/EditProfile' component={EditProfile} />
-                <Route path='/Profile' component={Profile} />
-                <Route path='/Profiles' component={Profiles} />
-                <Route path='/Post' component={Post} />
+    <Provider store={store}>
+        <BrowserRouter>
+            <div className='main_wrapper'>
+                <Switch>
+                    <Route path='/' component={Main} exact={true} />
+                    <Route path='/Login' component={LogIn} />
+                    <Route path='/SignUp' component={SignUp} />
+                    <Route path='/Dashboard' component={Dashboard} />
+                    <Route path='/AddEducation' component={AddEducation} />
+                    <Route path='/AddExperience' component={AddExperience} />
+                    <Route path='/Feed' component={Feed} />
+                    <Route path='/EditProfile' component={EditProfile} />
+                    <Route path='/Profile' component={Profile} />
+                    <Route path='/Profiles' component={Profiles} />
+                    <Route path='/Post' component={Post} />
 
 
-                <Route component={NotFound} />
-            </Switch>
-        </div>
+                    <Route component={NotFound} />
+                </Switch>
+            </div>
 
-    </BrowserRouter>
+         </BrowserRouter>
+    </Provider>
+
 );
 
 
