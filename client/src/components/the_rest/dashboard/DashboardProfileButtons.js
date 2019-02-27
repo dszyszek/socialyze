59d519afebbd13e64f763fbff089ec5d6a5e3370
Damian <37978110+dszyszek@users.noreply.github.com>
2019-02-27 01:21:17 +0100

    Add DashboardProfileButtons component

---
+++ client/src/components/the_rest/dashboard/DashboardProfileButtons.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const dashboardProfileButtons = () => (
+        <div class="btn-group mb-4" role="group">
+            <Link to="Profile" class="btn btn-light">
+            <i class="fas fa-user text-success mr-1"></i>
+            Your Profile</Link>
+
+            <Link to="EditProfile" class="btn btn-light">
+            <i class="fas fa-user-edit text-success mr-1"></i> Edit Profile</Link>
+
+            <Link to="AddExperience" class="btn btn-light">
+            <i class="fab fa-black-tie text-success mr-1"></i>
+            Add Experience</Link>
+            
+            <Link to="AddEducation" class="btn btn-light">
+            <i class="fas fa-graduation-cap text-success mr-1"></i>
+            Add Education</Link>
+      </div>
+);
+
+export default dashboardProfileButtons;
