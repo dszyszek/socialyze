8f2aab61c99ba8f06bce882e5bd2052469f72ade
Damian <37978110+dszyszek@users.noreply.github.com>
2019-03-04 02:19:22 +0100

    Make Profiles and ProfileBox components working properly (render profiles)

--- client/src/components/the_rest/profiles/ProfileBox.js
+++ client/src/components/the_rest/profiles/ProfileBox.js
@@ -1,39 +1,76 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import isEmpty from 'lodash/isEmpty';
 
 class ProfileBox extends React.Component {
-    render() {
-        return (
+    constructor() {
+        super();
+
+        this.fillProfiles = this.fillProfiles.bind(this);
+        this.fillSkillList = this.fillSkillList.bind(this);
+    }
+
+    fillSkillList(skills) {
+        const skillsArray = [];
+
+        skills.forEach(skill => {
+            skillsArray.push(
+                <li class="list-group-item">
+                <i class="fa fa-check pr-1"></i>{skill}</li>
+            )
+        })
+        return skillsArray;
+    }
+
+    fillProfiles() {
+        const {profiles} = this.props.profile;
+        const profilesArray = [];
+        console.log(profiles);
+
+        profiles.forEach(profile => {
+            profilesArray.push(
             <div class="card card-body bg-light mb-3">
                 <div class="row">
-                <div class="col-2">
-                    <img class="rounded-circle" src="https://www.gravatar.com/avatar/anything?s=153&d=mm" alt="" />
+                    <div class="col-2">
+                        <img class="rounded-circle" src="https://www.gravatar.com/avatar/anything?s=153&d=mm" alt="" />
+                    </div>
+                    <div class="col-lg-6 col-md-4 col-8">
+                        <h3>{profile.handle}</h3>
+                        
+                        {!isEmpty(profile.experience) ? <p>{profile.status} at {profile.experience[profile.experience.length - 1].company}</p>  : <p>Currently not employed</p>}
+
+                        {!isEmpty(profile.location) ? <p>{profile.location}</p> : <p>Location unknown</p>}
+                        <Link to="Profile" class="btn main_color">View Profile</Link>
+                    </div>
+                    <div class="col-md-4 d-none d-lg-block">
+                        <h4>Skill Set</h4>
+                        <ul class="list-group">
+
+                            {this.fillSkillList(profile.skills)}
+
+                        </ul>
+                    </div>
                 </div>
-                <div class="col-lg-6 col-md-4 col-8">
-                    <h3>Anonymous_1</h3>
-                    <p>Developer at NASA</p>
-                    <p>Warsaw, PL</p>
-                    <Link to="Profile" class="btn main_color">View Profile</Link>
-                </div>
-                <div class="col-md-4 d-none d-lg-block">
-                    <h4>Skill Set</h4>
-                    <ul class="list-group">
-                    <li class="list-group-item">
-                        <i class="fa fa-check pr-1"></i>HTML</li>
-                    <li class="list-group-item">
-                        <i class="fa fa-check pr-1"></i>CSS</li>
-                    <li class="list-group-item">
-                        <i class="fa fa-check pr-1"></i>JavaScript</li>
-                    <li class="list-group-item">
-                        <i class="fa fa-check pr-1"></i>Python</li>
-                    <li class="list-group-item">
-                        <i class="fa fa-check pr-1"></i>C#</li>
-                    </ul>
-                </div>
-                </div>
+            </div>
+            );
+            
+        });
+        return profilesArray;
+    }
+
+    render() {
+
+        return (
+            <div>
+                {this.props.profile.profiles ? this.fillProfiles() : ''} 
             </div>
         );
     }
 }
 
-export default ProfileBox;
+const mapStateToProps = state => ({
+    profile: state.profile
+});
+
+export default connect(mapStateToProps)(ProfileBox);
--- client/src/components/the_rest/profiles/Profiles.js
+++ client/src/components/the_rest/profiles/Profiles.js
@@ -17,7 +17,7 @@ class AddEducation extends React.Component {
     }
 
     componentWillReceiveProps(props) {
-        this.setState(prev => (console.log('updating'), {
+        this.setState(prev => ({
             ...prev,
             profiles: props.profile.profiles
         }));
@@ -34,42 +34,15 @@ class AddEducation extends React.Component {
 
                 <div class="profiles">
                     <div class="container">
-                    <div class="row">
-                        <div class="col-md-12">
-                        <h1 class="display-4 text-center">Profiles</h1>
-                        <p class="lead text-center">Browse and connect with other people</p>
-                
-                       <ProfileBox />
-                
-                        <div class="card card-body bg-light mb-3">
-                            <div class="row">
-                            <div class="col-2">
-                                <img class="rounded-circle" src="https://www.gravatar.com/avatar/anything?s=153&d=mm" alt="" />
-                            </div>
-                            <div class="col-lg-6 col-md-4 col-8">
-                                <h3>Anonymous_2</h3>
-                                <p>Developer at Google</p>
-                                <p>Wroclove, PL</p>
-                                <Link to="Profile" class="btn main_color">View Profile</Link>
-                            </div>
-                            <div class="col-md-4 d-none d-lg-block">
-                                <h4>Skill Set</h4>
-                                <ul class="list-group">
-                                <li class="list-group-item">
-                                    <i class="fa fa-check pr-1"></i>HTML</li>
-                                <li class="list-group-item">
-                                    <i class="fa fa-check pr-1"></i>CSS</li>
-                                <li class="list-group-item">
-                                    <i class="fa fa-check pr-1"></i>PHP</li>
-                                <li class="list-group-item">
-                                    <i class="fa fa-check pr-1"></i>MySQL</li>
-                                </ul>
-                            </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h1 class="display-4 text-center">Profiles</h1>
+                                <p class="lead text-center">Browse and connect with other people</p>
+                        
+                                <ProfileBox />
+                    
                             </div>
                         </div>
-                
-                        </div>
-                    </div>
                     </div>
               </div>
 
