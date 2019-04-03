9fc00cda7ef754def29e34826f89daf75ed76eba
Damian <37978110+dszyszek@users.noreply.github.com>
2019-04-03 22:58:02 +0200

    Prepare webpack configuration for deployment

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -22,10 +22,5 @@ module.exports = {
                 'sass-loader'
             ]
         }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
     }
 };
