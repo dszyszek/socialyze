147a4fd1e241f03bfc5c143a27d82008d4dd5b2c
Damian <37978110+dszyszek@users.noreply.github.com>
2019-04-05 18:52:21 +0200

    Fix: app crashes when page reloaded in any route

--- server/server.js
+++ server/server.js
@@ -40,6 +40,10 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, '..', 'client', 'public', 'index.html'));
   });
 
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'client', 'public', 'index.html'));
+  });
+
 }
 
 app.listen(port, () => {
