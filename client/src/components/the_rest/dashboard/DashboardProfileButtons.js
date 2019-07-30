169ca0449ae023ad90b36bea567b6e1e3663bc53
Damian <37978110+dszyszek@users.noreply.github.com>
2019-07-30 05:55:17 +0200

    Handle input[type="file"] element in different way

--- client/src/components/the_rest/dashboard/DashboardProfileButtons.js
+++ client/src/components/the_rest/dashboard/DashboardProfileButtons.js
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
 
-const dashboardProfileButtons = (props) => (
-        <div class="btn-group mb-4" role="group">
+class DashboardProfileButtons extends Component {
 
-            <Link to={`Profile/${props.currentProfile}`} class="btn btn-light">
-                  <i class="fas fa-user text-success mr-1"></i>Your Profile
-            </Link>
+      redirectClick = (e) => {
+            this.chooseFile.click();
+          }
 
-            <Link to="EditProfile" class="btn btn-light">
-                  <i class="fas fa-user-edit text-success mr-1"></i> Edit Profile
-            </Link>
+      render() {
+            return (
+                  <div class="btn-group mb-4" role="group">
 
-            <Link to="AddExperience" class="btn btn-light">
-                  <i class="fab fa-black-tie text-success mr-1"></i>Add Experience
-            </Link>
+                        <Link to={`Profile/${this.props.currentProfile}`} class="btn btn-light">
+                              <i class="fas fa-user text-success mr-1"></i>Your Profile
+                        </Link>
             
-            <Link to="AddEducation" class="btn btn-light">
-                  <i class="fas fa-graduation-cap text-success mr-1"></i>Add Education
-            </Link>
+                        <Link to="EditProfile" class="btn btn-light">
+                              <i class="fas fa-user-edit text-success mr-1"></i> Edit Profile
+                        </Link>
+            
+                        <Link to="AddExperience" class="btn btn-light">
+                              <i class="fab fa-black-tie text-success mr-1"></i>Add Experience
+                        </Link>
+                        
+                        <Link to="AddEducation" class="btn btn-light">
+                              <i class="fas fa-graduation-cap text-success mr-1"></i>Add Education
+                        </Link>
+                        
+                        <button class='btn btn-light' onClick={this.redirectClick}>
+                              <i class="fas fa-camera text-success mr-1"></i>Change Image
+                        </button>
+                        <input type='file' name='' class='btn btn-light' ref={input => this.chooseFile = input} style={{display: 'none'}} onChange={this.props.upload} />
+                  </div>
+            );
+      }
+}
 
-            <input type='file' class='btn btn-light' onChange={props.upload} />
-      </div>
-);
 
-export default dashboardProfileButtons;
+export default DashboardProfileButtons;
