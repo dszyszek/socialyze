83b5666f3a04e3aed6d5ec3aeeb30e8c81caed0b
Damian <37978110+dszyszek@users.noreply.github.com>
2019-08-05 23:01:28 +0200

    Fix: wrong navbar in NotFound component, if request from unauthorized user

--- client/src/components/the_rest/NotFound.js
+++ client/src/components/the_rest/NotFound.js
@@ -1,29 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 import Navbar_secondary from './Navbar_secondary';
+import Navbar_logged_out from './Navbar_logged_out';
 import Footer_main from './Footer_main';
 
-const NotFound = () => {
-    return (
-        <div class='main_wrapper'>
-            <Navbar_secondary/>
-            <div class='container'>
-                
-                <div class='jumbotron pl-0 pr-0 text-center mt-5'>
-                    <h1 style={{fontSize: '3em'}}>
-                        404 NOT FOUND
-                    </h1>
-                    <p>
-                        It seems like there is no such page :&#40;
-                    </p>
+
+class NotFound extends Component  {
+    render() {
+        return (
+            <div class='main_wrapper'>
+                {this.props.auth.auth ? <Navbar_secondary/> : <Navbar_logged_out />}
+                <div class='container'>
+                    
+                    <div class='jumbotron pl-0 pr-0 text-center mt-5'>
+                        <h1 style={{fontSize: '3em'}}>
+                            404 NOT FOUND
+                        </h1>
+                        <p>
+                            It seems like there is no such page :&#40;
+                        </p>
+                    </div>
+
                 </div>
-
+                <Footer_main/>
             </div>
-            <Footer_main/>
-        </div>
-    );
+        );
+    }
 };
 
-export {
-    NotFound as default
-};
+
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+
+export default connect(mapStateToProps)(NotFound);
